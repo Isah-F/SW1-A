aa63bbd36df06957438259611215fa0efe157724
Isah_F <ksiohmf1@gmail.com>
2026-03-31 23:46:25 -0300

    31-03

--- Lista de exercício - PHP/Exercício 8/login.php
+++ Lista de exercício - PHP/Exercício 8/login.php
@@ -1,10 +1,10 @@
 <?php
-    $nome = $_POST["usuario"];
+    $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
 
-    if($nome = "isabela" && $senha = "1234"){
+    if($usuario == "isabela" && $senha == "1234"){
         echo"<h3>Acesso permitido.</h3>";
-        echo"<p>Seja bem vinda, ".$nome."!</p>";
+        echo"<p>Seja bem vinda, ".$usuario."!</p>";
     }else{
         echo"<h3>Erro: Acesso negado.</h3>";
         echo"<p>Informação incorreta.</p>";
