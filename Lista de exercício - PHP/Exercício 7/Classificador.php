8c851013f576d17e130e59ec19107a391f7b5168
Isah_F <ksiohmf1@gmail.com>
2026-03-31 23:39:04 -0300

    31-03

--- Lista de exercício - PHP/Exercício 7/Classificador.php
+++ Lista de exercício - PHP/Exercício 7/Classificador.php
@@ -3,7 +3,7 @@
     $nota2 = $_POST["nota2"];
     $media = ($nota1 + $nota2)/2;
 
-    echo"<h2>Resultado média: ".$media"</h2>";
+    echo"<h2>Resultado média: ".$media."</h2>";
     if($media >=7){
         echo"<b>Aprovado!</b>";
     }elseif ($media >=5 && $media <7){
